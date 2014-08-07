9cf97cd19a595d1b92f742735bfb6015d3fc8799
rupy <rupyapps@gmail.com>
2014-08-07 23:58:59 +0900

    forest fire modelのネットワークによるファイル保存処理

--- snap_test.py
+++ snap_test.py
@@ -1,3 +1,5 @@
+#coding: utf-8
+
 from snap import *
 
 # サンプルから
@@ -12,7 +14,7 @@ G1.AddEdge(5,1)
 G1.AddEdge(5,32)
 
 
-# 有効ランダムぐらふの生成（100ノード、1000辺）
+# 有効ランダムグラフの生成（100ノード、1000辺）
 G2 = GenRndGnm(PNGraph, 100, 1000)
 # ノードの列挙
 for NI in G2.Nodes():
@@ -26,4 +28,16 @@ for NI in G2.Nodes():
     for Id in NI.GetOutEdges():
         print "edge (%d %d)" % (NI.GetId(), Id)
 
+
+# Forest Fire modelを用いたネットワーク
+G3 = GenForestFire(1000, 0.35, 0.35)
+# バイナリを保存＆読み込み
+FOut = TFOut("test.graph")
+G3.Save(FOut)
+FOut.Flush()
+FIn = TFIn("test.graph")
+G4 = TNGraph.Load(FIn)
+# 保存してテキストファイルとして読み込む
+SaveEdgeList(G4, "test.txt", "Save as tab-separated list of edges")
+G5 = LoadEdgeList(PNGraph, "test.txt", 0, 1)
 
